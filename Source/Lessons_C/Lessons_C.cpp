b3db6136585ec20e8682122611abff3ea9b22a70
SiTiROS <sitiros666@gmail.com>
2024-01-08 22:16:47 +0600

    Lesson 5 and update ue 5.3

---
+++ Source/Lessons_C/Lessons_C.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Lessons_C.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Lessons_C, "Lessons_C" );
